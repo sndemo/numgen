32a53188a5f215d9591da57c38ccb36bea688c99
Kamran Ali Khan <khankamranali@gmail.com>
2018-08-26 23:04:41 +0530

    working version

--- numgen_rest.py
+++ numgen_rest.py
@@ -2,8 +2,8 @@ import json
 import requests
 from requests.auth import HTTPBasicAuth
 
-#API_HTTP="seldonapi.aws.s12n.tk"
-API_HTTP="localhost:5000"
+API_HTTP="seldonapi.aws.s12n.tk"
+#API_HTTP="localhost:5000"
 
 def get_token():
     payload = {'grant_type': 'client_credentials'}
@@ -18,40 +18,16 @@ def get_token():
 def rest_request():
     token = get_token()
     headers = {'Authorization': 'Bearer '+token}
-    payload = {"data":{"names":["a","b"],"tensor":{"shape":[2,2],"values":[0,0,1,1]}}}
+    payload = {"data":{"names":["x"],"ndarray":[[[20.0],[21.0],[22.0],[23.0],[24.0]]]}}
     response = requests.post(
                 "http://"+API_HTTP+"/api/v0.1/predictions",
                 headers=headers,
                 json=payload)
     print(response.text)
 
-def gen_REST_request(batch,features,tensor=True):
-    if tensor:
-        datadef = {
-            "names":features,
-            "tensor":{
-                    "shape":batch.shape,
-                    "values":batch.ravel().tolist()
-                    }
-            }
-    else:
-        datadef = {
-            "names":features,
-            "ndarray":batch.tolist()
-            }
-        
-    request = {
-        "meta":{},
-        "data":datadef
-        }
-
-    return request
-
 def rest_request_without_oauth():
-    #payload = {"data":{"names":["x"],"tensor":{"shape":[1,5,1],"values":[[[20],[21],[22],[23],[24]]]}}}
     payload = {"data":{"names":["x"],"ndarray":[[[20.0],[21.0],[22.0],[23.0],[24.0]]]}}
-    #response = requests.post("http://"+API_HTTP+"/predict",json=payload)
     response = requests.post("http://"+API_HTTP+"/predict", data={"json":json.dumps(payload)})
     print(response.text)
 
-rest_request_without_oauth()
+rest_request()
